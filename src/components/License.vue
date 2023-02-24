4001933cb8a81c7b8ac4c54b5b76fdeac4db21df
Lunar Requiem <lunarrequiem42@gmail.com>
2023-02-25 00:05:24 +0100

    Format links in the Paizo Community Use Policy

--- src/components/License.vue
+++ src/components/License.vue
@@ -2,7 +2,7 @@
 	<div class="license-wrapper">
 		<h2>Paizo Inc. Community Use Policy</h2>
 		<p>
-			This website uses trademarks and/or copyrights owned by Paizo Inc., used under Paizo's Community Use Policy (paizo.com/communityuse). We are expressly prohibited from charging you to use or access this content. This website is not published, endorsed, or specifically approved by Paizo. For more information about Paizo Inc. and Paizo products, visit paizo.com.
+			This website uses trademarks and/or copyrights owned by Paizo Inc., used under Paizo's Community Use Policy	(<a href="https://paizo.com/communityuse">paizo.com/communityuse</a>). We are expressly prohibited from charging you to use or access this content. This website is not published, endorsed, or specifically approved by Paizo. For more information about Paizo Inc. and Paizo products, visit <a href="https://paizo.com">paizo.com</a>.
 		</p>
 		<h2>OPEN GAME LICENSE Version 1.0a</h2>
 		<p>
